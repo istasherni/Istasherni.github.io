182cd34f5eba2f3e8db56d2881d53ef6f5ff4506
istasherni <ismailmahamood19+istasherni@gmail.com>
2024-12-01 09:00:20 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8be9f04565e4ffe9689bace808bd1501",
+const RESOURCES = {"flutter_bootstrap.js": "b6c4d1c1e3c9e286d5ce79df648776b7",
 "version.json": "60d23d36440f2c621526fc98e91586d9",
 "index.html": "5fe987d659d7dd7d3e6819d53a448bf0",
 "/": "5fe987d659d7dd7d3e6819d53a448bf0",
-"main.dart.js": "a40ca2bf2f4ae898075c02e5f13aab6f",
+"main.dart.js": "4cf9b42acac96bb9a0d190a375d8b367",
 "istasherni_logo.png": "4d0f9170b94b50d8053f8a2b24200168",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
